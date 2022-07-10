47048d0f8517ec276e2ff4f8718e37196757a328
Eduardo Mazzucco Cividini <edumazzucco@hotmail.com>
2022-07-10 20:33:00 -0300

    feat: planets route

--- api.ts
+++ api.ts
@@ -1,9 +1,14 @@
 import { Router } from "https://deno.land/x/oak@v10.6.0/mod.ts";
+import * as planets from "./models/planets.ts";
 
 const router = new Router();
 
 router.get("/", (ctx) => {
   ctx.response.body = "Hello World!";
+});
+
+router.get("/planets", (ctx) => {
+  ctx.response.body = planets.getAllPlanets();
 });
 
 export default router;
