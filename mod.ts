5773e69e0cc82bb3de9ebee1f9c69c95a30219f1
Eduardo Mazzucco Cividini <edumazzucco@hotmail.com>
2022-07-10 19:30:04 -0300

    feat: added logs and performance measurement

--- mod.ts
+++ mod.ts
@@ -3,6 +3,21 @@ import { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
 const app = new Application();
 const port = 8000;
 
+app.use(async (ctx, next) => {
+  await next();
+  const time = ctx.response.headers.get("X-Response-Time");
+  console.log(
+    `${ctx.request.method} ${ctx.request.url} - ${ctx.response.status}: ${time}`,
+  );
+});
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const delta = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${delta}ms`);
+});
+
 app.use((ctx) => {
   ctx.response.body = "Hello World!";
 });
